3bf041036647ee328fd9e25c2efdf13dfccaa220
Horacio Coronel <arielc@gmail.com>
2021-01-15 15:03:03 -0500

    Add Dashboard styling, add modal and nav menu

---
+++ components/modal/addSite.js
@@ -0,0 +1,132 @@
+const AddSiteModal = ({ closeAddSiteModal }) => {
+  return (
+    <div class="fixed z-10 inset-0 overflow-y-auto">
+      <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        {/* <!--
+      Background overlay, show/hide based on modal state.
+
+      Entering: "ease-out duration-300"
+        From: "opacity-0"
+        To: "opacity-100"
+      Leaving: "ease-in duration-200"
+        From: "opacity-100"
+        To: "opacity-0"
+    --> */}
+        <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+          <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+        </div>
+
+        {/* <!-- This element is to trick the browser into centering the modal contents. --> */}
+        <span
+          class="hidden sm:inline-block sm:align-middle sm:h-screen"
+          aria-hidden="true"
+        >
+          &#8203;
+        </span>
+        {/* <!--
+      Modal panel, show/hide based on modal state.
+
+      Entering: "ease-out duration-300"
+        From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        To: "opacity-100 translate-y-0 sm:scale-100"
+      Leaving: "ease-in duration-200"
+        From: "opacity-100 translate-y-0 sm:scale-100"
+        To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+    --> */}
+        <div
+          class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full w-full"
+          role="dialog"
+          aria-modal="true"
+          aria-labelledby="modal-headline"
+        >
+          <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div class="sm:flex sm:items-start">
+              <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-indigo-50 sm:mx-0 sm:h-10 sm:w-10">
+                <svg
+                  className="w-6 h-6 text-indigo-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
+                <h3
+                  class="text-lg leading-6 font-medium text-gray-900"
+                  id="modal-headline"
+                >
+                  Add Site
+                </h3>
+                <div class="mt-2">
+                  {/* <p class="text-sm text-gray-500">
+                    Are you sure you want to deactivate your account? All of
+                    your data will be permanently removed. This action cannot be
+                    undone.
+                  </p> */}
+                  <div className="mt-6">
+                    <label
+                      htmlFor="name"
+                      class="block text-sm text-left font-medium text-gray-700"
+                    >
+                      Site Name
+                    </label>
+                    <div class="mt-1">
+                      <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                        placeholder="Your Site Name"
+                      />
+                    </div>
+                  </div>
+                  <div className="mt-3">
+                    <label
+                      htmlFor="url"
+                      class="block text-sm text-left font-medium text-gray-700"
+                    >
+                      Site URL
+                    </label>
+                    <div class="mt-1">
+                      <input
+                        type="text"
+                        name="url"
+                        id="url"
+                        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                        placeholder="https://wwww.google.com"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <button
+              type="button"
+              class="w-full inline-flex justify-center rounded-full border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+            >
+              Create
+            </button>
+            <button
+              type="button"
+              class="mt-3 w-full inline-flex justify-center rounded-full border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+              onClick={closeAddSiteModal}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddSiteModal;
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,344 @@
+import { useState } from "react";
+import { useAuth } from "@/lib/auth";
+import { Transition } from "@headlessui/react";
+import ProfileImage from "@/components/dashboard/profileImage";
+import Router from "next/router";
+import { useRouter } from "next/router";
+import AddSiteModal from "@/components/modal/addSite";
+
 const Dashboard = () => {
+  const auth = useAuth();
+  const user = auth.user;
+  const [isOpen, setIsOpen] = useState(false);
+  const [addSiteModal, setAddSiteModal] = useState(false);
+  const router = useRouter();
+
+  const handleMenu = (e) => {
+    e.preventDefault();
+    setIsOpen(!isOpen);
+  };
+
+  const openAddSiteModal = (e) => {
+    e.preventDefault();
+    setAddSiteModal(true);
+  };
+
+  const closeAddSiteModal = (e) => {
+    e.preventDefault();
+    setAddSiteModal(false);
+  };
+
   return (
-    <div>
-      This is the Dashboard
-      <br />
-      Options
+    <div className="min-h-screen bg-white">
+      <nav className="bg-white border-b border-gray-200">
+        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="flex justify-between h-16">
+            <div className="flex">
+              <div className="flex items-center flex-shrink-0">
+                <img
+                  className="block w-auto h-8 lg:hidden"
+                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                  alt="Workflow"
+                />
+                <img
+                  className="hidden w-auto h-8 lg:block"
+                  src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
+                  alt="Workflow"
+                />
+              </div>
+              <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
+                {/* <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" --> */}
+                <a
+                  href="#"
+                  className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-900 border-b-2 border-indigo-500"
+                >
+                  Sites
+                </a>
+
+                <a
+                  href="#"
+                  className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-gray-300 hover:text-gray-700"
+                >
+                  Feedback
+                </a>
+              </div>
+            </div>
+            <div className="hidden sm:ml-6 sm:flex sm:items-center">
+              <button className="p-1 text-gray-400 bg-white rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <span className="sr-only">View notifications</span>
+                {/* <!-- Heroicon name: bell --> */}
+                <svg
+                  className="w-6 h-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                  />
+                </svg>
+              </button>
+
+              <div className="relative ml-3">
+                <div>
+                  <button
+                    className="flex items-center max-w-xs text-sm bg-white rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    id="user-menu"
+                    aria-haspopup="true"
+                    onClick={handleMenu}
+                  >
+                    <span className="sr-only">Open user menu</span>
+                    <ProfileImage usrImage={auth?.user?.photoUrl} />
+                  </button>
+                </div>
+                <Transition
+                  show={isOpen}
+                  enter="transition ease-out duration-200"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <div
+                    className="absolute right-0 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5"
+                    role="menu"
+                    aria-orientation="vertical"
+                    aria-labelledby="user-menu"
+                  >
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      role="menuitem"
+                    >
+                      Your Profile
+                    </a>
+
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      role="menuitem"
+                    >
+                      Settings
+                    </a>
+
+                    <a
+                      href="#"
+                      onClick={(e) => auth.signout(e)}
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      role="menuitem"
+                    >
+                      Sign out
+                    </a>
+                  </div>
+                </Transition>
+              </div>
+            </div>
+            <div className="flex items-center -mr-2 sm:hidden">
+              {/* <!-- Mobile menu button --> */}
+              <button
+                className="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                onClick={handleMenu}
+              >
+                <span className="sr-only">Open main menu</span>
+                {/* <!--
+              Heroicon name: menu
+
+              Menu open: "hidden", Menu closed: "block"
+            --> */}
+                <svg
+                  className="block w-6 h-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+                {/* <!--
+              Heroicon name: x
+
+              Menu open: "block", Menu closed: "hidden"
+            --> */}
+                <svg
+                  className="hidden w-6 h-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* <!--
+      Mobile menu, toggle classes based on menu state.
+
+      Open: "block", closed: "hidden"
+    --> */}
+        <Transition
+          show={isOpen}
+          enter="transition ease-out duration-200"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <div className={`${isOpen ? "block" : "hidden"} sm:hidden`}>
+            <div className="pt-2 pb-3 space-y-1">
+              {/* <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800" --> */}
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-base font-medium text-indigo-700 border-l-4 border-indigo-500 bg-indigo-50"
+              >
+                Sites
+              </a>
+
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-base font-medium text-gray-600 border-l-4 border-transparent hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+              >
+                Feedback
+              </a>
+            </div>
+            <div className="pt-4 pb-3 border-t border-gray-200">
+              <div className="flex items-center px-4">
+                <div className="flex-shrink-0">
+                  <ProfileImage usrImage={auth?.user?.photoUrl} />
+                </div>
+                <div className="ml-3">
+                  <div className="text-base font-medium text-gray-800">
+                    {user?.name ? user.name : user.email}
+                  </div>
+                  <div className="text-sm font-medium text-gray-500">
+                    {user?.name ? user.email : ""}
+                  </div>
+                </div>
+                <button className="flex-shrink-0 p-1 ml-auto text-gray-400 bg-white rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                  <span className="sr-only">View notifications</span>
+                  {/* <!-- Heroicon name: bell --> */}
+                  <svg
+                    className="w-6 h-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div className="mt-3 space-y-1">
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
+                >
+                  Your Profile
+                </a>
+
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
+                >
+                  Settings
+                </a>
+
+                <a
+                  href="#"
+                  onClick={(e) => auth.signout(e)}
+                  className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
+                >
+                  Sign out
+                </a>
+              </div>
+            </div>
+          </div>
+        </Transition>
+      </nav>
+
+      <div className="py-10 bg-blue-50">
+        <header>
+          <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold leading-tight text-gray-900">
+              Sites
+            </h1>
+          </div>
+        </header>
+        <main>
+          <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            {/* <!-- Replace with your content --> */}
+            <div className="px-4 py-8 sm:px-0">
+              <div className="bg-white border-gray-100 rounded shadow border-1">
+                <div className="p-10 text-center">
+                  <h2 className="font-semibold">
+                    You haven't added any sites yet
+                  </h2>
+                  <p className="mb-4 text-sm text-gray-600">
+                    Welcome {user.name ? user.name : user.email}! Let's get
+                    started.
+                  </p>
+                  <button
+                    type="button"
+                    class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-full text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    onClick={openAddSiteModal}
+                  >
+                    Add your first site
+                    {/* <!-- Heroicon name: mail --> */}
+                    <svg
+                      class="h-5 w-5 ml-2"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                      />
+                    </svg>
+                  </button>
+                  {addSiteModal ? (
+                    <AddSiteModal closeAddSiteModal={closeAddSiteModal} />
+                  ) : (
+                    ""
+                  )}
+                </div>
+              </div>
+            </div>
+            {/* <!-- /End replace --> */}
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
